1dfef2fa22e53aebb8c287398c8e3d5aa8312888
Tree <71885233+Appl3Tree@users.noreply.github.com>
2021-10-06 22:24:07 +0000

    Fixed missing Day 2 BMI calculator.

--- 100 Days of Code/Day 002 - Data Types and How to Manipulate Strings/day-2-2-exercise.py
+++ 100 Days of Code/Day 002 - Data Types and How to Manipulate Strings/day-2-2-exercise.py
@@ -1,2 +1,8 @@
 #!/usr/bin/env python3
 
+height = float(input('Enter your height in m: '))
+weight = float(input('Enter your weight in kg: '))
+
+bmi = int(weight / (height ** 2))
+
+print('Your bmi is ' + str(bmi))
