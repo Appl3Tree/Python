8b6e66d8a120ce7342a6e23f2faee02a3e0fac93
Tree <71885233+Appl3Tree@users.noreply.github.com>
2021-10-12 15:41:44 +0000

    Finished easy version of final project for Day 5.

--- 100 Days of Code/Day 005 - Python Loops/password-generator.py
+++ 100 Days of Code/Day 005 - Python Loops/password-generator.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+#   Modules
+import random
+
+#   Variables
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '^', '(', ')', '*', '+']
+password = ''
 
 #   Banner
 print('''
@@ -6,3 +14,19 @@ print('''
 *                     Welcome to the PyPassword Generator                     *
 *******************************************************************************
 ''')
+nr_letters = int(input('How many letters would you like in your password?\n\t'))
+nr_symbols = int(input('How many symbols would you like in your password?\n\t'))
+nr_numbers = int(input('How many numbers would you like in your password?\n\t'))
+totalChars = nr_letters + nr_symbols + nr_numbers
+
+#   Easy
+for l in range(nr_letters):
+    password += random.choice(letters)
+for s in range(nr_symbols):
+    password += random.choice(symbols)
+for n in range(nr_numbers):
+    password += random.choice(numbers)
+print(f'Here is your password: {password}')
+
+#   Hard
+password = ''
