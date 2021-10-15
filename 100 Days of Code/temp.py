2c6db3b406f250e713289cb5c82efe2be2947165
Tree <71885233+Appl3Tree@users.noreply.github.com>
2021-10-15 19:47:03 +0000

    Learning about positional vs. keyword arguments.

---
+++ 100 Days of Code/temp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#def greet(name):
+#    print(f'Hello, {name}.')
+#    print('How are you?')
+#   print('I\'m doing well.')
+
+#name = input('What\'s your name?\n')
+#greet(name)
+
+def greet_with(name, location):
+    print(f'Hello, {name}.')
+    print(f'What is it like in {location}?')
+
