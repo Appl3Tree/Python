e5814926e128aa512121c3f726463a4b5866a555
Tree <71885233+Appl3Tree@users.noreply.github.com>
2021-10-15 19:33:06 +0000

    Created Day 8.

---
+++ 100 Days of Code/Day 008 - Function Parameters & Caesar Cipher/caesar-cipher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+logo = '''
+
+'''
+coding = input('Type \'encode\' to encrypt, or type \'decode\' to decrypt.\n').lower()
+if coding == 'encode':
+    message = input('Type your message:\n')
+    shift = input('Type the shift number:\n')
+    # TODO: Finish encoding.
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
 import os
 os.system('cp /home/runner/Python/pynew /opt/virtualenvs/python3/bin/pynew')
-#print('The command \'pynew\' is now available for use.\n')
-os.system('"100 Days of Code/Day 007 - Hangman/hangman.py"')
+print('The command \'pynew\' is now available for use.\n')
