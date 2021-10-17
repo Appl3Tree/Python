2bf5a593d6403739f1bb3c309eef356092af197a
Forrest Connelly <forrest1995@yahoo.com>
2021-10-17 09:55:15 -0600

    Day 8 exercise 1 & 2. Started Caesar Cipher.

---
+++ 100 Days of Code/Day 008 - Function Parameters & Caesar Cipher/day-8-1-exercise.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+
+def paint_calc(height, width, cover):
+    numberOfCans = math.ceil(height * width / cover)
+    print(f'You will need {numberOfCans} cans of paint.')
+
+test_h = int(input('Height of wall: '))
+test_w = int(input('Width of wall: '))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
