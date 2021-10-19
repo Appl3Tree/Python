f2174ca344612af63e55a34f81021c591791c7fb
Forrest Connelly <forrest1995@yahoo.com>
2021-10-19 08:49:35 -0600

    Cleaned up code with Angela.

--- 100 Days of Code/Day 009 - Dictionaries, Nesting and the Secret Auction/blind-auction.py
+++ 100 Days of Code/Day 009 - Dictionaries, Nesting and the Secret Auction/blind-auction.py
@@ -2,28 +2,42 @@
 from art import logo
 from os import name, system
 
+#   Basic function to clear screen depending on OS.
 def clear():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
+
+#   Function to determine highest bidder.
+def findHighestBidder(biddingRecord):
+    highestBid = 0
+    for bidder in biddingRecord:
+        bidAmount = biddingRecord[bidder]
+        if bidAmount > highestBid:
+            highestBid = bidAmount
+            winner = bidder
+    highestBid = "{:.2f}".format(highestBid)
+    print(f'The winner of the silent auction is {winner} with a bid of ${highestBid}.')
+
+#   Clear screen and display blind auction logo.
 clear()
 print(logo)
-bids = []
-moreBidders = 'yes'
-while moreBidders != 'no':
-    print('Welcome to the secret auction program.')
-    bidderName = input('What is your name?: ')
-    bid = float(input('What\'s your bid?: $'))
-    bids.append({'Name': bidderName, 'Bid': bid})
-    moreBidders = input('Are there any other bidders? Type \'yes\' or \'no\'.\n')
-    clear()
 
-highestBid = 0
-winner = ''
-for bidder in bids:
-    if bidder['Bid'] > highestBid:
-        winner = bidder['Name']
-        highestBid = bidder['Bid']
-highestBid = "{:.2f}".format(highestBid)
-print(f'The winner of the blind auction is {winner} with a bid of ${highestBid}.\n')
+#   Declare necessary variables.
+bids = {}
+biddingFinished = False
+
+while not biddingFinished:
+    print('Welcome to the secret auction program.')
+    name = input('What is your name?: ')
+    price = float(input('What\'s your bid?: $'))
+    bids[name] = price
+    shouldContinue = input('Are there any other bidders? Type \'yes\' or \'no\'.\n')
+    if shouldContinue == 'no':
+        biddingFinished = True
+        clear()
+        print(logo)
+        findHighestBidder(bids)
+    else:
+        clear()
