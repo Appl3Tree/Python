6d7351bdc361b930c9c7ac035811d11c1b79f765
Forrest Connelly <forrest1995@yahoo.com>
2021-10-30 13:40:44 -0600

    updated day 12 project

--- 100 Days of Code/Day 012 - Scope and Number Guessing Game/guess-the-number.py
+++ 100 Days of Code/Day 012 - Scope and Number Guessing Game/guess-the-number.py
@@ -3,39 +3,53 @@ from random import randint
 from os import name, system
 from art import logo
 
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
+
 def clear():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
 
-gameOver = False
-while not gameOver:
+def check_answer(guess, answer, turns):
+    '''Checks answer against guess. Returns the number of attempts remaining.'''
+    if guess > answer:
+        print('Too high.')
+        return turns - 1
+    elif guess < answer:
+        print('Too low.')
+        return turns - 1
+    else:
+        print(f'You got it! The answer was {answer}.')
+
+def set_difficulty():
+    level = input('Choose a difficulty. Type \'easy\' or \'hard\': ')
+    if level == 'hard':
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
     clear()
     print(logo)
     print('Welcome to the Number Guessing Game!')
     print('I\'m thinking of a number between 1 and 100.')
-    num = randint(1,100)
-    difficulty = input('Choose a difficulty. Type \'easy\' or \'hard\': ')
-    if difficulty == 'hard':
-        attempts = 5
-    else:
-        attempts = 10
-    while attempts > 0:
+    answer = randint(1,100)
+
+    attempts = set_difficulty()
+
+    guess = 0
+    while guess != answer:
         print(f'You have {attempts} attempts remaining to guess the number.')
         guess = int(input('Make a guess: '))
-        if guess != num:
-            attempts -= 1
-            if guess > num:
-                print('Too high.')
-            else:
-                print('Too low.')
-            print('Guess again.')
-        else:
-            print(f'You got it! The answer was {num}')
+        attempts = check_answer(guess, answer, attempts)
+        if attempts == 0:
+            print('You\'ve run out of guesses, you lose. The number was {num}.')
             break
-    if attempts == 0:
-        print('You\'ve run out of guesses, you lose. The number was {num}.')
     keepPlaying = input('\nWould you like to play again? Type \'y\' or \'n\': ')
-    if keepPlaying == 'n':
-        gameOver = True
+    if keepPlaying == 'y':
+        game()
+
+game()
