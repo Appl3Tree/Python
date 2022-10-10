7b57614f159825496295f316377fdf6be8bced79
Forrest Connelly <forrest1995@yahoo.com>
2022-10-09 18:20:43 -0700

    Updated to work with data from the Open Triva DB API.

--- 100 Days of Code/Day 017 - The Quiz Project/main.py
+++ 100 Days of Code/Day 017 - The Quiz Project/main.py
@@ -15,8 +15,8 @@ def clear():
 clear()
 question_bank = []
 for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question['question']
+    question_answer = question['correct_answer']
     new_question = Question(text=question_text, answer=question_answer)
     question_bank.append(new_question)
 
