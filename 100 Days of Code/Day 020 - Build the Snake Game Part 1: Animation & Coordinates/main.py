21c49dac7d9492c3f309e104bed4934f7101e13f
Forrest Connelly <forrest1995@yahoo.com>
2022-10-13 19:15:41 -0700

    Wrote Day 20.

--- 100 Days of Code/Day 020 - Build the Snake Game Part 1: Animation & Coordinates/main.py
+++ 100 Days of Code/Day 020 - Build the Snake Game Part 1: Animation & Coordinates/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 from os import name, system
+from turtle import Screen  # , Turtle
+from snake import Snake
+import time
 
 
 def clear():
@@ -8,4 +11,67 @@ def clear():
     else:
         system('clear')
 
+# Task #1
+# screen = Screen()
+# screen.setup(width=650, height=650)
+# screen.screensize(canvwidth=600, canvheight=600)
+# screen.bgcolor('black')
+# screen.title('My Snake Game')
+# screen.tracer(0)
+#
+# snake = []
+# # Initial snake body
+# for i in range(3):
+#     body_chunk = Turtle()
+#     body_chunk.shape('square')
+#     body_chunk.color('white')
+#     body_chunk.penup()
+#     body_chunk.setposition(x=(i * -20), y=0)
+#     snake.append(body_chunk)
+# screen.update()
+# screen.listen()
+#
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.1)
+#     for chunk_num in range(len(snake) - 1, 0, -1):
+#         new_x = snake[chunk_num - 1].xcor()
+#         new_y = snake[chunk_num - 1].ycor()
+#         snake[chunk_num].goto(new_x, new_y)
+#     snake[0].forward(20)
+#     screen.onkeypress(fun=go_east, key='d')
+#     screen.onkeypress(fun=go_north, key='w')
+#     screen.onkeypress(fun=go_west, key='a')
+#     screen.onkeypress(fun=go_south, key='s')
 
+
+# Task #2
+clear()
+screen = Screen()
+screen.setup(width=650, height=650)
+screen.screensize(canvwidth=600, canvheight=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+snake = Snake()
+screen.listen()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    screen.onkeypress(snake.right, 'Right')
+    screen.onkeypress(snake.right, 'd')
+    screen.onkeypress(snake.up, 'Up')
+    screen.onkeypress(snake.up, 'w')
+    screen.onkeypress(snake.left, 'Left')
+    screen.onkeypress(snake.left, 'a')
+    screen.onkeypress(snake.down, 'Down')
+    screen.onkeypress(snake.down, 's')
+
+
+screen.exitonclick()
---
+++ 100 Days of Code/Day 020 - Build the Snake Game Part 1: Animation & Coordinates/snake.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+from os import name, system
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+RIGHT = 0
+UP = 90
+LEFT = 180
+DOWN = 270
+
+def clear():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+
+class Snake:
+
+    def __init__(self):
+        self.body = []
+        self.create_snake()
+        self.head = self.body[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            body_part = Turtle()
+            body_part.shape('square')
+            body_part.color('white')
+            body_part.penup()
+            body_part.setposition(position)
+            self.body.append(body_part)
+
+    def move(self):
+        for chunk_num in range(len(self.body) - 1, 0, -1):
+            new_x = self.body[chunk_num - 1].xcor()
+            new_y = self.body[chunk_num - 1].ycor()
+            self.body[chunk_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
