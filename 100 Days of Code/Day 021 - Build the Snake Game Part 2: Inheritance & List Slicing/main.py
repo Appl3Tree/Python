dbfcc01b250cbdb05ecb0809e95edc0c5b8a1866
Forrest Connelly <forrest1995@yahoo.com>
2022-10-13 19:18:48 -0700

    Removed commented code from Day 20.

--- 100 Days of Code/Day 021 - Build the Snake Game Part 2: Inheritance & List Slicing/main.py
+++ 100 Days of Code/Day 021 - Build the Snake Game Part 2: Inheritance & List Slicing/main.py
@@ -11,42 +11,7 @@ def clear():
     else:
         system('clear')
 
-# Task #1
-# screen = Screen()
-# screen.setup(width=650, height=650)
-# screen.screensize(canvwidth=600, canvheight=600)
-# screen.bgcolor('black')
-# screen.title('My Snake Game')
-# screen.tracer(0)
-#
-# snake = []
-# # Initial snake body
-# for i in range(3):
-#     body_chunk = Turtle()
-#     body_chunk.shape('square')
-#     body_chunk.color('white')
-#     body_chunk.penup()
-#     body_chunk.setposition(x=(i * -20), y=0)
-#     snake.append(body_chunk)
-# screen.update()
-# screen.listen()
-#
-# game_is_on = True
-# while game_is_on:
-#     screen.update()
-#     time.sleep(0.1)
-#     for chunk_num in range(len(snake) - 1, 0, -1):
-#         new_x = snake[chunk_num - 1].xcor()
-#         new_y = snake[chunk_num - 1].ycor()
-#         snake[chunk_num].goto(new_x, new_y)
-#     snake[0].forward(20)
-#     screen.onkeypress(fun=go_east, key='d')
-#     screen.onkeypress(fun=go_north, key='w')
-#     screen.onkeypress(fun=go_west, key='a')
-#     screen.onkeypress(fun=go_south, key='s')
 
-
-# Task #2
 clear()
 screen = Screen()
 screen.setup(width=650, height=650)
