1a29b711a6885a21ee5d20dba9678d36dfea2201
Forrest Connelly <forrest1995@yahoo.com>
2022-10-17 13:17:04 -0700

    Reverted changes back to pre-Day 24.

--- 100 Days of Code/Day 021 - Build the Snake Game Part 2: Inheritance & List Slicing/scoreboard.py
+++ 100 Days of Code/Day 021 - Build the Snake Game Part 2: Inheritance & List Slicing/scoreboard.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import turtle
 from os import name, system
 from turtle import Turtle
 ALIGNMENT = 'center'
@@ -18,8 +17,6 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open(file='data.txt', mode='r') as data:
-            self.high_score = int(data.read())
         self.hideturtle()
         self.penup()
         self.goto(0, 300)
@@ -28,16 +25,12 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f'Score: {self.score} High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
+        self.write(f'Score: {self.score}', align=ALIGNMENT, font=FONT)
 
     def add_point(self):
         self.score += 1
         self.update_scoreboard()
 
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            with open('data.txt', 'w') as data:
-                data.write(f'{self.score}')
-        self.score = 0
-        self.update_scoreboard()
+    def game_over(self):
+        self.goto(0, 0)
+        self.write('GAME OVER', align='center', font=FONT)
