c946607a03fd5383ad25f2ea817017d25f32bac8
Forrest Connelly <forrest1995@yahoo.com>
2022-10-22 18:26:52 -0700

    Fixed light/dark mode swapping and updated widths of widgets to be a cleaner output.

--- 100 Days of Code/Day 029 - Building a Password Manager GUI App with Tkinter/main.py
+++ 100 Days of Code/Day 029 - Building a Password Manager GUI App with Tkinter/main.py
@@ -20,7 +20,7 @@ MODE_FONT = BLACK
 
 # ---------------------------- MODE TOGGLE------------------------------- #
 def toggle_mode():
-    global MODE_BG, MODE_FONT, password_button, add_button
+    global MODE_BG, MODE_FONT, generate_password_button, add_button
     if mode_state.get():
         MODE_BG = BLACK
         MODE_FONT = WHITE
@@ -34,16 +34,33 @@ def toggle_mode():
         password_entry.config(bg=MODE_BG, fg=MODE_FONT)
         light_mode_toggle.config(bg=MODE_BG, fg=MODE_FONT)
         dark_mode_toggle.config(bg=MODE_BG, fg=MODE_FONT)
-        password_button.destroy()
+        generate_password_button.destroy()
         add_button.destroy()
-        password_button = Button(text='Generate Password', width=12, bg=MODE_BG, fg=MODE_FONT,
-                                 highlightbackground=MODE_BG)
-        password_button.grid(column=2, row=3)
+        generate_password_button = Button(text='Generate Password', width=12, bg=MODE_BG, fg=MODE_FONT,
+                                          highlightbackground=MODE_BG)
+        generate_password_button.grid(column=2, row=3)
         add_button = Button(text='Add', width=36, bg=MODE_BG, fg=MODE_FONT, highlightbackground=MODE_BG)
         add_button.grid(column=1, row=4, columnspan=2)
     else:
         MODE_BG = WHITE
         MODE_FONT = BLACK
+        window.config(bg=MODE_BG)
+        canvas.config(bg=MODE_BG)
+        website_label.config(bg=MODE_BG, fg=MODE_FONT)
+        website_entry.config(bg=MODE_BG, fg=MODE_FONT)
+        email_user_label.config(bg=MODE_BG, fg=MODE_FONT)
+        email_user_entry.config(bg=MODE_BG, fg=MODE_FONT)
+        password_label.config(bg=MODE_BG, fg=MODE_FONT)
+        password_entry.config(bg=MODE_BG, fg=MODE_FONT)
+        light_mode_toggle.config(bg=MODE_BG, fg=MODE_FONT)
+        dark_mode_toggle.config(bg=MODE_BG, fg=MODE_FONT)
+        generate_password_button.destroy()
+        add_button.destroy()
+        generate_password_button = Button(text='Generate Password', width=12, bg=MODE_BG, fg=MODE_FONT,
+                                          highlightbackground=MODE_BG)
+        generate_password_button.grid(column=2, row=3)
+        add_button = Button(text='Add', width=36, bg=MODE_BG, fg=MODE_FONT, highlightbackground=MODE_BG)
+        add_button.grid(column=1, row=4, columnspan=2)
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -53,40 +70,42 @@ def toggle_mode():
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20, bg=MODE_BG)
+window.config(padx=50, pady=50, bg=MODE_BG)
 
 canvas = Canvas(width=200, height=200, bg=MODE_BG, highlightthickness=0)
 logo_img = PhotoImage(file='logo.png')
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(column=1, row=0)
 
+# Labels
 website_label = Label(text='Website:', bg=MODE_BG, fg=MODE_FONT)
 website_label.grid(column=0, row=1, sticky=E)
-website_entry = Entry(width=35, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
-website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
-website_entry.focus_set()
-
 email_user_label = Label(text='Email/Username:', bg=MODE_BG, fg=MODE_FONT)
 email_user_label.grid(column=0, row=2, sticky=E)
-email_user_entry = Entry(width=35, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
-email_user_entry.grid(column=1, row=2, columnspan=2, sticky=W)
-
 password_label = Label(text='Password:', bg=MODE_BG, fg=MODE_FONT)
 password_label.grid(column=0, row=3, sticky=E)
-password_entry = Entry(width=21, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
-password_entry.grid(column=1, row=3, sticky=W)
-password_button = Button(text='Generate Password', width=12, bg='blue', fg='red', highlightbackground=green)
-password_button.grid(column=2, row=3)
 
+# Mode Radiobuttons
 mode_state = IntVar()
 light_mode_toggle = Radiobutton(text='Light Mode', value=0, bg=MODE_BG, fg=MODE_FONT, variable=mode_state,
                                 command=toggle_mode)
 dark_mode_toggle = Radiobutton(text='Dark Mode', value=1, bg=MODE_BG, fg=MODE_FONT, variable=mode_state,
                                command=toggle_mode)
-light_mode_toggle.grid(column=0, row=4, sticky=W)
-dark_mode_toggle.grid(column=0, row=5, sticky=W)
+light_mode_toggle.grid(column=0, row=5, sticky=W)
+dark_mode_toggle.grid(column=0, row=6, sticky=W)
 
+# Entries
+website_entry = Entry(width=39, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
+website_entry.grid(column=1, row=1, columnspan=2, sticky=W)
+website_entry.focus_set()
+email_user_entry = Entry(width=39, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
+email_user_entry.grid(column=1, row=2, columnspan=2, sticky=W)
+password_entry = Entry(width=21, bg=MODE_BG, fg=MODE_FONT, highlightthickness=0)
+password_entry.grid(column=1, row=3, sticky=W)
 
+# Buttons
+generate_password_button = Button(text='Generate Password', bg=MODE_BG, fg=MODE_FONT, highlightbackground=MODE_BG)
+generate_password_button.grid(column=2, row=3)
 add_button = Button(text='Add', width=36, bg=MODE_BG, fg=MODE_FONT, highlightbackground=MODE_BG)
 add_button.grid(column=1, row=4, columnspan=2)
 
